fc66cc912e913dc19401276fc640b84bf16c4c44
mohkhan26 <mohammed.khan2@publicissapient.com>
2020-05-08 15:18:03 +0530

    trigger release

--- src/ApplicationGateway.UI.Catalog/Startup.cs
+++ src/ApplicationGateway.UI.Catalog/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace ApplicationGateway.UI.Catalog
-{
+{ 
     public class Startup
     {
         public Startup(IConfiguration configuration)
